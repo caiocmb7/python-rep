1b9e388a686e674143618c2da778eb2327220f67
Caio Barros <caiobarrosufc@gmail.com>
2021-09-16 17:45:11 -0300

    updated timer project

--- projetos/2. Timer/timer.py
+++ projetos/2. Timer/timer.py
@@ -2,12 +2,15 @@ import time
 
 def contador_tempo(t):
     while t:
-        minutos, segundos = divmod(t, 60) #receber o tempo e dividir em 60 para identificar min, seg
+        #receber o tempo e dividir em 60 para identificar min, seg
+        minutos, segundos = divmod(t, 60) 
         timer = "{:02d}:{:02d}".format(minutos, segundos)
-        print(timer, end="\r") #printar na msm linha
+
+        #printar na msm linha
+        print(timer, end="\r") 
         time.sleep(1)
         t -= 1
-    print("\n Time completed!!")
+    print("\n Contagem realizada")
 
 if __name__ == "__main__":
     t = input("Digite um valor em segundos: ")
