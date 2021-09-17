16debf5a84876e6374be80b265f13148b437188b
Caio Barros <caiobarrosufc@gmail.com>
2021-09-17 17:55:47 -0300

    updated readme

---
+++ projetos/3. jpg_to_png/jpg_to_png.py
@@ -0,0 +1,14 @@
+from PIL import Image 
+import os
+
+def png_to_jpg():
+    input_path = str(input("Qual o caminho da pasta: "))
+    for filename in os.listdir(input_path):
+        current_img = Image.open(input_path + '\\' + filename)
+        current_img.save(input_path + '\\' +
+                        filename + '.png', 'PNG')
+
+    print("Finalizado!")
+
+if __name__ == "__main__":
+    png_to_jpg()
