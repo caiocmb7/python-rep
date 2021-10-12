2c99855af1e9b4aded4e400544c38ebb8a0ca1ee
Caio Barros <caiobarrosufc@gmail.com>
2021-10-12 20:50:28 -0300

    finished carro

--- oop/oop-basics/carro.py
+++ oop/oop-basics/carro.py
@@ -1,5 +1,6 @@
 import string
 import random 
+from random import randint
 
 class Carro:
     def __init__(self, marca, potencia, km_por_litro):
@@ -7,24 +8,32 @@ class Carro:
         self.potencia = potencia
         self.km_por_litro = km_por_litro
 
+
     def exibirInfo(self):
         print(f"\nA marca do carro é: {self.marca}")
         print(f"\nA potencia do carro é: {self.potencia}")
-        print(f"\nA marca do carro é: {self.km_por_litro}")
+        print(f"\nO carro faz: {self.km_por_litro}")
     
     def definirPlaca(self):
-        string.ascii_letters
-        l1 = random.choice(string.ascii_letters)
-        l2.upper()
-        l2 = random.choice(string.ascii_letters)
-        l2.upper()
-        l3 = random.choice(string.ascii_letters)
-        l3.upper()
+        l1 = random.choice(string.ascii_letters).upper()
+        l2 = random.choice(string.ascii_letters).upper()
+        l3 = random.choice(string.ascii_letters).upper()
 
-        n1 = randint(0,9)
-        n2 = randint(0,9)
-        n3 = randint(0,9)
+        n1 = str(randint(0,9))
+        n2 = str(randint(0,9))
+        n3 = str(randint(0,9))
 
         placa = l1 + l2 + l3 + "-" + n1 + n2 + n3
+        return f"\nA placa do carro será: {placa}"
+    
+    def definirCor(self):
+        lista = ['Rosa', 'Amarelo', 'Azul', 'Preto', 'Branco', 'Cinza', 'Verde', 'Roxo', 'Marrom']
+        chute = random.choice(lista)
+        return f"\nA cor do carro será: {chute}"
+        
+carro_teste = Carro('Toyota', '1200 cavalos', '10 km/L')
+print(carro_teste.exibirInfo())
+print(carro_teste.definirPlaca())
+print(carro_teste.definirCor())
 
 
