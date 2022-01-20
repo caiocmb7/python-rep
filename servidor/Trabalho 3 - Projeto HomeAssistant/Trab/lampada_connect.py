8ee5879e8b6444f9d7dbc8f25794859a6b1bbb2c
Caio Barros <caiobarrosufc@gmail.com>
2022-01-20 10:26:00 -0300

    lampada_connect

--- servidor/Trabalho 3 - Projeto HomeAssistant/Trab/lampada_connect.py
+++ servidor/Trabalho 3 - Projeto HomeAssistant/Trab/lampada_connect.py
@@ -11,9 +11,9 @@ channel.exchange_declare(exchange='lum_sensor', exchange_type='direct')
 
 while True:
     alt = random.random() * random.randrange(-5, 5, 1) # modifica valores em um range especifico
-    intensidade = 15 + alt
+    luminosidade = 15 + alt
     channel.basic_publish(
-        exchange='', routing_key='lampada', body=str(intensidade))
+        exchange='', routing_key='lampada', body=str(luminosidade))
     time.sleep(5)
 
 connection.close()
